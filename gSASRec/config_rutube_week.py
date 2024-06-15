4eafbaf6602fadde50bc478be604e955f9686195
M1croZavr <tlashukov@gmail.com>
2024-06-15 21:25:18 +0300

    New gsasrec config

--- gSASRec/config_rutube_week.py
+++ gSASRec/config_rutube_week.py
@@ -6,12 +6,12 @@ from gSASRec.config import GSASRecExperimentConfig
 config = GSASRecExperimentConfig(
     dataset_name='rutube_week',
     sequence_length=50,
-    embedding_dim=50,
+    # embedding_dim=50,
     num_heads=1,
     max_batches_per_epoch=100,
     num_blocks=2,
     dropout_rate=0.5,
-    negs_per_pos=100,
+    # negs_per_pos=100,
     gbce_t=0.75,
     metrics=[
         Precision @ 1, Precision @ 5, Precision @ 10,
